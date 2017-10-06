9c1e1b55ff2af1a335714c6cef2a7c676d95e58a
Aluno <Aluno@172.32.1.28>
2017-10-05 21:56:33 -0300

    criação de função com retorno e criação de um pacote com uma classe de exemplo

--- src/github/Github.java
+++ src/github/Github.java
@@ -2,11 +2,21 @@ package github;
 
 public class Github {
 
-
+public static void quadrado(double n){
+    double r = n * n;
+    
+    
+}
+    
+    
 public static void main(String[] args) {
 
+    double raiz;
+    raiz = Math.sqrt(15);
+    
+    System.out.println("A raiz é: " + raiz);
+    
 
-
-    }
+}
     
 }
