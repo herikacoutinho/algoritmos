c0ad38940cd632203df76f5bb51d4f4b750dccd4
Aluno <Aluno@172.32.1.28>
2017-10-05 22:25:16 -0300

    criação de função pedir número

--- src/exemploFuncao/FuncaoComRetorno.java
+++ src/exemploFuncao/FuncaoComRetorno.java
@@ -9,6 +9,14 @@ import java.util.Scanner;
 
 public class FuncaoComRetorno {
 
+    public static double pedirNumero() {
+        Scanner e = new Scanner(System.in);
+        System.out.println("Digite um número: ");
+        double num = e.nextDouble();
+
+        return num;
+    }
+
     public static void criarMenu() {
 
         //Colocar o que vai aconteer ao executar
@@ -22,7 +30,6 @@ public class FuncaoComRetorno {
         System.out.println("0. Sair");
         System.out.println("--------------------");
 
-      
     }
 
     public static void exemploTipo1() {
@@ -33,41 +40,32 @@ public class FuncaoComRetorno {
     }
 
     public static void calcularDobro() {
-        Scanner e = new Scanner(System.in);
         double n, dobro;
-        System.out.print("Digite um número: ");
-        n = e.nextDouble();
+        n = pedirNumero();
         dobro = n * 2;
         System.out.println("Dobro de " + n + " = " + dobro);
     }
 
     public static void somar2Numeros() {
-        Scanner e = new Scanner(System.in);
-        int n, o, soma;
-        System.out.print("Digite um número: ");
-        n = e.nextInt();
-        System.out.print("Digite outro número: ");
-        o = e.nextInt();
+        double n, o, soma;
+        n = pedirNumero();
+        o = pedirNumero();
         soma = o + n;
         System.out.println("A soma de " + n + " + " + o + " = " + soma);
     }
 
     public static void raizQuadrada() {
-        Scanner e = new Scanner(System.in);
         double n, resultado;
-        System.out.print("Digite um número: ");
-        n = e.nextInt();
+        n = pedirNumero();
         resultado = Math.sqrt(n);
         System.out.println("A raiz quadrada de " + n + " = " + resultado);
     }
 
     public static void calcQuadrado() {
-        Scanner e = new Scanner(System.in);
         double n, resultado;
-        System.out.print("Digite um número: ");
-        n = e.nextInt();
+        n = pedirNumero();
         resultado = Math.pow(n, 2);
-        System.out.println("A raiz quadrada de " + n + " = " + resultado);
+        System.out.println("O Dobro é " + resultado);
     }
 
     public static void calcTab(int x) {
